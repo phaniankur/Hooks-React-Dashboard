aef952e69cb997d165886ca29db8d4a3aca48007
Ankur Phani <contact.ankurphani@gmail.com>
2021-05-18 00:55:36 +0530

    First Input Components Added

--- src/pages/Gridlayout.js
+++ src/pages/Gridlayout.js
@@ -29,30 +29,33 @@ const useStyles = makeStyles((theme) => ({
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
             <Paper className={classes.paper}>
-                <h1>Track Orders</h1>
+                <h1>Title</h1>
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper className={classes.paper}>
-                <h1>Submitted Files</h1>
+                <h1>Media</h1>
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper className={classes.paper}>
-                <h1>Active Subscriptions</h1>
+                <h1>Author/Host</h1>
             </Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <h1>Track Order</h1></Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
+          
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <h1>Artwork</h1>
+            </Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <h1>Subscription</h1>
+            </Paper>
           </Grid>
         </Grid>
       </div>
